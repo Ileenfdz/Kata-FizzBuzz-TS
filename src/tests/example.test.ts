c20c411f6f18b3ce3015e06665af707885425637
Ileenfdz <ileenfdz@gmail.com>
2021-12-21 13:36:05 +0100

    Added new test and tried it to do an error, and then, get a success

--- src/tests/example.test.ts
+++ src/tests/example.test.ts
@@ -1,7 +1,11 @@
-import { sum } from "../ts/Example";
+// import { sum } from "../ts/Example";
+import { multiply } from "../ts/Example";
 
 describe('Just an example to test jest installation', () => {
+    // test('should be two', () => {
+    //     expect(sum(1, 1)).toBe(2);
+    // });
     test('should be two', () => {
-        expect(sum(1, 1)).toBe(2);
+        expect(multiply(2, 2)).toBe(4);
     });
 });
--- src/ts/Example.ts
+++ src/ts/Example.ts
@@ -1,4 +1,8 @@
-export function sum(a:number, b:number) : number
+// export function sum(a:number, b:number) : number
+// {
+//     return a + b;
+// }
+export function multiply(a:number, b:number) : number
 {
     return a + b;
 }
